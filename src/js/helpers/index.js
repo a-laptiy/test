c549fc2ab2a51256dd1ad937d3caf99d79dc3260
Alex L <alex.l.04604@gmail.com>
2020-01-28 12:45:19 +0600

    init

--- src/js/helpers/index.js
+++
@@ -1,9 +0,0 @@
-const select = function select(elem) {
-  return document.querySelector(elem);
-};
-
-const selectAll = function selectAll(elem) {
-  return Array.from(document.querySelectorAll(elem));
-};
-
-export { select, selectAll };
